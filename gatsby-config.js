721f4fc68fbbdd3c7678cb95d8ef5a5da393b565
rotijoe <joseph@islandsocialclub.com>
2021-08-23 14:37:04 +0100

    installed deps fpr gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: "@chakra-ui/gatsby-plugin",
       options: {
