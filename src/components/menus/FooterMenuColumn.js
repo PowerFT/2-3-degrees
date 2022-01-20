ada1a136069c84f432447aed38cee6b653c62a7a
rotijoe <joseph@islandsocialclub.com>
2022-01-20 12:28:45 +0000

    fixing footer menu 2

--- src/components/menus/FooterMenuColumn.js
+++ src/components/menus/FooterMenuColumn.js
@@ -1,11 +1,95 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
-import { Link } from 'gatsby';
+import { graphql, Link, StaticQuery } from 'gatsby';
 import { Box, Stack, Text } from '@chakra-ui/react';
 
-const FOOTER_MENU_QUERY = gql`
-  query FooterMenuQuery($id: ID!) {
-    menu(id: $id) {
+export const FooterMenuColumn = ({ id, ...rest }) => {
+  // const { data, loading, error } = useQuery(FOOTER_MENU_QUERY, {
+  //   variables: {
+  //     id: id,
+  //   },
+  // });
+  // console.log(id);
+
+  // if (error) console.log(error);
+  // if (!data || loading) return null;
+  // if (data) console.log(data, id);
+
+  // // console.log(data, id);
+  // // if (data.menu?.menuItems) {
+  // //   console.log(data.menu?.menuItems);
+  // const menuItems = data.menu?.menuItems?.nodes;
+
+  return (
+    <StaticQuery
+      query={FOOTER_MENU_QUERY}
+      render={(data) => {
+        if (data.wpMenu) {
+          const menuItems = data.menu?.menuItems?.nodes;
+
+          return (
+            <>
+              {menuItems ? (
+                <Box key={data.menu.id} {...rest}>
+                  <Text fontWeight="bold" mb="4">
+                    {data.menu.name}
+                  </Text>
+                  <Stack as="ul" listStyleType="none">
+                    {menuItems?.map((link, idx) => (
+                      <Box as="li" key={idx} mt="1" mb="0">
+                        <Box
+                          as={Link}
+                          to={link.path}
+                          _hover={{
+                            textDecor: 'underline',
+                          }}
+                        >
+                          {link.label}
+                        </Box>
+                      </Box>
+                    ))}
+                  </Stack>
+                </Box>
+              ) : (
+                <></>
+              )}
+            </>
+          );
+        }
+      }}
+    />
+  );
+};
+
+//   return menuItems ? (
+//     <Box key={data.menu.id} {...rest}>
+//       <Text fontWeight="bold" mb="4">
+//         {data.menu.name}
+//       </Text>
+//       <Stack as="ul" listStyleType="none">
+//         {menuItems?.map((link, idx) => (
+//           <Box as="li" key={idx} mt="1" mb="0">
+//             <Box
+//               as={Link}
+//               to={link.path}
+//               _hover={{
+//                 textDecor: 'underline',
+//               }}
+//             >
+//               {link.label}
+//             </Box>
+//           </Box>
+//         ))}
+//       </Stack>
+//     </Box>
+//   ) : (
+//     <></>
+//   );
+// };
+
+const FOOTER_MENU_QUERY = graphql`
+  query FooterMenuQuery($id: String!) {
+    wpMenu(id: { eq: $id }) {
       id
       name
       menuItems {
@@ -17,46 +101,3 @@ const FOOTER_MENU_QUERY = gql`
     }
   }
 `;
-
-export const FooterMenuColumn = ({ id, ...rest }) => {
-  const { data, loading, error } = useQuery(FOOTER_MENU_QUERY, {
-    variables: {
-      id: id,
-    },
-  });
-  console.log(id);
-
-  if (error) console.log(error);
-  if (!data || loading) return null;
-  if (data) console.log(data, id);
-
-  // console.log(data, id);
-  // if (data.menu?.menuItems) {
-  //   console.log(data.menu?.menuItems);
-  const menuItems = data.menu?.menuItems?.nodes;
-
-  return menuItems ? (
-    <Box key={data.menu.id} {...rest}>
-      <Text fontWeight="bold" mb="4">
-        {data.menu.name}
-      </Text>
-      <Stack as="ul" listStyleType="none">
-        {menuItems?.map((link, idx) => (
-          <Box as="li" key={idx} mt="1" mb="0">
-            <Box
-              as={Link}
-              to={link.path}
-              _hover={{
-                textDecor: 'underline',
-              }}
-            >
-              {link.label}
-            </Box>
-          </Box>
-        ))}
-      </Stack>
-    </Box>
-  ) : (
-    <></>
-  );
-};
